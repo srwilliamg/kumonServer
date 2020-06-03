9fe4d03405835f9b6f379bc174af3c0518719f5e
William Salazar <srwilliamg@gmail.com>
2020-06-03 17:57:28 -0500

    updates

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,15 +1,13 @@
 var Parent = require('../models/index').parent;
-var JwtStrategy = require('passport-jwt').Strategy, ExtractJwt = require('passport-jwt').ExtractJwt;
+var JwtStrategy = require('passport-jwt').Strategy, 
+ExtractJwt = require('passport-jwt').ExtractJwt;
 
 module.exports = function (passport) {
   var opts = {}
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
   opts.secretOrKey = 'OtherSecret';
-  // opts.issuer = 'accounts.examplesoft.com';
-  // opts.audience = 'yoursite.net';
   passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
     // console.log(jwt_payload);
-    
     Parent.findByPk(jwt_payload.id_parent)
     .then(data => {
         return done(null, data);
